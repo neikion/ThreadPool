e5734545bca4e7d3b69f46aaf7cb17ea9b77f1be
neikion <neikion43@gmail.com>
2024-03-31 23:43:49 +0900

    update TreahdPoolMain

--- ThreadPoolMain.cpp
+++ ThreadPoolMain.cpp
@@ -7,58 +7,61 @@
 #include <functional>
 #include <future>
 
-
 namespace ThreadPool {
-	extern "C" {
-		class ThreadPool {
-		public:
-			std::mutex AccessJob;
-			std::queue<std::function<void()>> works;
-			size_t limit;
-			std::vector<std::thread> workers;
-			std::condition_variable AccessNoti;
-			bool shutdown;
-			~ThreadPool() {
-				shutdown = true;
-				AccessNoti.notify_all();
-				for (size_t i = 0; i < workers.size(); i++) {
-					workers[i].join();
-				}
-			}
-			void worker() {
-				while (!shutdown) {
-					using namespace std;
-					unique_lock<mutex> ul(AccessJob);
-					AccessNoti.wait(ul, [self = this]() {return !self->works.empty() || self->shutdown; });
-					if (shutdown) {
-						return;
-					}
-					auto work = move(works.front());
-					works.pop();
-					ul.unlock();
-					work();
-				}
-			}
-			void readyWorks() {
-				workers.reserve(limit);
-				for (size_t i = 0; i < workers.size(); i++) {
-					workers.push_back(std::thread(ThreadPool::worker));
-				}
-			}
-			ThreadPool() : shutdown(false),limit(std::thread::hardware_concurrency()) {
-				readyWorks();
-			}
-			ThreadPool(size_t size) : limit(size),shutdown(false) {
-				readyWorks();
-			}
-			/*
-			template은 extern "C"와 함께 사용하지 못한다.
-			맹글링을 사용하지 못하기 때문이다.
-			template<typename T,typename... args>
-			std::future<std::invoke_result_t<T, args>> push() {
 
+	class ThreadPool {
+	public:
+		std::mutex AccessJob;
+		std::queue<std::function<void()>> works;
+		size_t limit;
+		std::vector<std::thread> workers;
+		std::condition_variable AccessNoti;
+		bool shutdown;
+		~ThreadPool() {
+			shutdown = true;
+			AccessNoti.notify_all();
+			for (size_t i = 0; i < workers.size(); i++) {
+				workers[i].join();
 			}
-			*/
-		};
+		}
+		void worker() {
+			while (!shutdown) {
+				using namespace std;
+				unique_lock<mutex> ul(AccessJob);
+				AccessNoti.wait(ul, [self = this]() {return !self->works.empty() || self->shutdown; });
+				if (shutdown) {
+					return;
+				}
+				auto work = move(works.front());
+				works.pop();
+				ul.unlock();
+				work();
+			}
+		}
+		void readyWorks() {
+			workers.reserve(limit);
+			for (size_t i = 0; i < workers.size(); i++) {
+				workers.push_back(std::thread(&ThreadPool::worker,this));
+			}
+		}
+		ThreadPool() : shutdown(false), limit(std::thread::hardware_concurrency()) {
+			readyWorks();
+		}
+		ThreadPool(size_t size) : limit(size), shutdown(false) {
+			readyWorks();
+		}
+		//extern "C"를 사용하기 위해서는
+		//push_void_intArg1(void (*work)(), int arg1);
+		//과 같이 정확한 타입을 명시해줘야한다.(C에서는 함수 오버로드가 안된다.)
+		template<typename T,typename... args>
+		std::future<std::invoke_result_t<T, args...>> push(T work,args... value) {
+			
+		}
+
+
+	};
+
+	void ThreadMain() {
+
 	}
 }
--- ThreadPoolMain.h
+++ ThreadPoolMain.h
@@ -1,6 +1,4 @@
 #pragma once
 namespace ThreadPool {
-	void ThreadMain() {
-
-	}
+	void ThreadMain();
 }
